0b63fb18ba03a515c4836b272c77fa7df0b4d755
Sandeep <Sandyb2711@gmail.com>
2024-02-15 01:19:33 +0530

    practice of actuator

---
+++ src/main/java/com/infy/ConfigurationClass.java
@@ -0,0 +1,12 @@
+package com.infy;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class ConfigurationClass
+{
+	
+
+}
