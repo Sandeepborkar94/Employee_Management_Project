49a14766c0c6e92a28eb0204198eb47d6210561e
Sandeep <Sandyb2711@gmail.com>
2024-02-15 01:21:07 +0530

    practice of actuator-httptrace added

--- src/main/java/com/infy/ConfigurationClass.java
+++ src/main/java/com/infy/ConfigurationClass.java
@@ -1,5 +1,6 @@
 package com.infy;
 
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,6 +8,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ConfigurationClass
 {
-	
+	@Bean
+	public InMemoryHttpExchangeRepository createTraceRepository() 
+	{
+	  return new InMemoryHttpExchangeRepository();
+	}
 
 }
